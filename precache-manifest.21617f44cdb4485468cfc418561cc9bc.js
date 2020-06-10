2ab56a02751050ffe618fe22c24b70effd9ab4cf
nanda-mik <nandamikushubha@gmail.com>
2020-06-10 22:27:38 +0530

    Updates

---
+++ precache-manifest.21617f44cdb4485468cfc418561cc9bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21b80be56b0c0e1174a5c51d233b0ab",
+    "url": "/zombie-rolodex/index.html"
+  },
+  {
+    "revision": "f8216e44786ec2ebe52f",
+    "url": "/zombie-rolodex/static/css/main.731e213c.chunk.css"
+  },
+  {
+    "revision": "ba6dbeb30beba9fac3a7",
+    "url": "/zombie-rolodex/static/js/2.24db0491.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zombie-rolodex/static/js/2.24db0491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8216e44786ec2ebe52f",
+    "url": "/zombie-rolodex/static/js/main.0358ab7f.chunk.js"
+  },
+  {
+    "revision": "bf3bd70f05cbf75b2161",
+    "url": "/zombie-rolodex/static/js/runtime-main.031e1498.js"
+  }
+]);
